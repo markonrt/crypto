85d238218f85d96a14452c4f4565f136c122c6b4
markonrt <mmihajlovic001@gmail.com>
2024-01-05 23:15:16 +0100

    nav bar edit

--- js/dynamic_page.js
+++ js/dynamic_page.js
@@ -1,9 +1,16 @@
-let dropDownMenu = document.querySelector('.dropDownMenuNot')
-let rotatingMenu = document.querySelector('.hamburger-menu')
-let toggleMenu = () => {
-    rotatingMenu.classList.toggle("rotate")
-    dropDownMenu.classList.toggle("dropDownMenu") 
-}
+let cryptoNavBtn = document.querySelector('.crypto')
+let openHam = document.querySelector('#openHam');
+let closeHam = document.querySelector('#closeHam');
+let nav_middle = document.querySelector('.nav-middle');
+
+const hamburgerEvent = (navigation, close, open) => {
+    nav_middle.style.display = navigation;
+    closeHam.style.display = close;
+    openHam.style.display = open;
+};
+
+openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
+closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
 
 const showData = document.querySelector('#dynamic-container')
         function getQueryParams() {
--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-let dropDownMenu = document.querySelector('.dropDownMenuNot')
-let rotatingMenu = document.querySelector('.hamburger-menu')
-let toggleMenu = () => {
-    rotatingMenu.classList.toggle("rotate")
-    dropDownMenu.classList.toggle("dropDownMenu") 
-}
+let cryptoNavBtn = document.querySelector('.crypto')
+let openHam = document.querySelector('#openHam');
+let closeHam = document.querySelector('#closeHam');
+let nav_middle = document.querySelector('.nav-middle');
+
+const hamburgerEvent = (navigation, close, open) => {
+    nav_middle.style.display = navigation;
+    closeHam.style.display = close;
+    openHam.style.display = open;
+};
+
+openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
+closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
+
+
 function redirectToDynamicPage(num){
   const dynamicPageUrl = `dynamic_page.html?id=${num}`;
 
