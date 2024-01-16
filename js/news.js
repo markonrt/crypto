5b88bad5f990b50fbc319208cfd53133e58913a7
markonrt <mmihajlovic001@gmail.com>
2024-01-16 23:37:35 +0100

    news page update

--- js/news.js
+++ js/news.js
@@ -4,23 +4,46 @@ function redirectToDynamicPage(num){
     // Redirect to the dynamically created page
     window.location.href = dynamicPageUrl;
   }
-  
+ var categoryArray = [];  
   const grid = document.querySelector('.grid')
-    const cardTemplate = document.getElementById('card-template')
-    for (let i = 0; i < 20; i++) {
-      grid.append(cardTemplate.content.cloneNode(true))
-    }
-    
     fetch("https://api.npoint.io/97488dd289e1f449c689")
       .then(res => res.json())
       .then(posts => {
+
         grid.innerHTML = ''
         posts.forEach((post,num) => {
-          const div = cardTemplate.content.cloneNode(true)
-          div.querySelector('.header-img').setAttribute("src", `${post.img}`);
-          div.querySelector('[data-heading]').textContent = post.heading
-          div.querySelector('[data-resume]').textContent = post.resume
-          div.querySelector('[button]').innerHTML = `<div id="dynamicLink" onclick="redirectToDynamicPage(${num})">Read More!</div>`
-          grid.append(div)
-        })
+          grid.innerHTML += `
+            <div class="card" data-category="${post.category}">
+              <img class="header-img skeleton" src=${post.img}/>
+              <div class="categoryTitle" category>${post.category}</div>
+              <div class="title" data-heading>${post.heading}</div>
+              <div data-resume>${post.resume}</div>
+              <div id="dynamicLink" onclick="redirectToDynamicPage(${num})">Read More!</div>
+            </div>
+          `;  
   })
+})
+
+function categorySort(button){
+  
+  let sortCattegory = button.getAttribute('data-category')
+  let newsCattegory = document.querySelectorAll('.card')
+
+  newsCattegory.forEach((news)=>{
+    news.style.display = 'none'
+  })
+
+  if(sortCattegory === 'all'){
+    newsCattegory.forEach((news)=>{
+      news.style.display = 'flex'
+    })
+  }
+
+  newsCattegory.forEach((news)=>{
+    if(news.getAttribute('data-category').includes(sortCattegory)){
+      news.style.display = 'flex';
+    }
+  })
+}
+
+
