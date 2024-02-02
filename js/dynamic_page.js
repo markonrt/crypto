8ad2485510bd93861c4ee48ad19f9ed93faf4d33
markonrt <mmihajlovic001@gmail.com>
2024-02-03 00:17:51 +0100

    slight changes

--- js/dynamic_page.js
+++ js/dynamic_page.js
@@ -2,6 +2,7 @@ let cryptoNavBtn = document.querySelector('.crypto')
 let openHam = document.querySelector('#openHam');
 let closeHam = document.querySelector('#closeHam');
 let nav_middle = document.querySelector('.nav-middle');
+const showData = document.querySelector('#dynamic-container')
 
 const hamburgerEvent = (navigation, close, open) => {
     nav_middle.style.display = navigation;
@@ -12,32 +13,25 @@ const hamburgerEvent = (navigation, close, open) => {
 openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
 closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
 
-const showData = document.querySelector('#dynamic-container')
-        function getQueryParams() {
-            let value;
+function getQueryParams() {
+    let value;
             const queryString = window.location.search.substring(1);
             value = queryString.split('=')[1];
             return value;
-        }
+}
+async function displayForwardedData() {
+    const queryParams = getQueryParams();
+    const jsonData = await fetch(`https://api.npoint.io/97488dd289e1f449c689/${queryParams}`);
+    const data = await jsonData.json();
+    showData.innerHTML += `
+        <div class="imgBg">
+            <img src="${data.img}" class="dynamic_img">
+        </div>
+        <h2 class="dynamic_heading width">${data.heading}</h2>
+        `;
+    data.paragraphs.map(paragraph => {
+        showData.innerHTML += `<p class="dynamic_paragraph width">${paragraph}</p>`
+    })
+}
 
-        async function displayForwardedData() {
-            const queryParams = getQueryParams();
-            
-            // Fetch the JSON data based on the forwarded ID
-            const jsonData = await fetch(`https://api.npoint.io/97488dd289e1f449c689/${queryParams}`);
-            const data = await jsonData.json();
-            
-            showData.innerHTML += `
-                <div class="imgBg">
-                    <img src="${data.img}" class="dynamic_img">
-                </div>
-                
-                <h2 class="dynamic_heading width">${data.heading}</h2>
-            `;
-            
-            data.paragraphs.map(paragraph => {
-                showData.innerHTML += `<p class="dynamic_paragraph width">${paragraph}</p>`
-            })
-            
-        }
 displayForwardedData();
--- js/news.js
+++ js/news.js
@@ -1,15 +1,25 @@
-function redirectToDynamicPage(num){
-    const dynamicPageUrl = `dynamic_page.html?id=${num}`;
-  
-    // Redirect to the dynamically created page
-    window.location.href = dynamicPageUrl;
-  }
- var categoryArray = [];  
-  const grid = document.querySelector('.grid')
+const loader = document.querySelector("#loading");
+const grid = document.querySelector('.grid')
+
+function displayLoading() {
+  loader.classList.add("display");
+  loader.style.marginTop = "15rem";
+  loader.style.marginBottom = "15rem";
+  setTimeout(() => {
+      loader.classList.remove("display");
+  }, 25000);
+}
+function hideLoading() {
+  loader.classList.remove("display");
+  loader.style.marginTop = "0px";
+  loader.style.marginBottom = "0px";
+  loader.style.width = "0px";
+  loader.style.height = "0px";
+}
+displayLoading()
     fetch("https://api.npoint.io/97488dd289e1f449c689")
       .then(res => res.json())
       .then(posts => {
-
         grid.innerHTML = ''
         posts.forEach((post,num) => {
           grid.innerHTML += `
@@ -20,25 +30,22 @@ function redirectToDynamicPage(num){
               <div data-resume>${post.resume}</div>
               <div id="dynamicLink" onclick="redirectToDynamicPage(${num})">Read More!</div>
             </div>
-          `;  
+          `;
+          hideLoading();  
   })
 })
 
-function categorySort(button){
-  
+function categorySort(button){  
   let sortCattegory = button.getAttribute('data-category')
   let newsCattegory = document.querySelectorAll('.card')
-
   newsCattegory.forEach((news)=>{
     news.style.display = 'none'
   })
-
   if(sortCattegory === 'all'){
     newsCattegory.forEach((news)=>{
       news.style.display = 'flex'
     })
   }
-
   newsCattegory.forEach((news)=>{
     if(news.getAttribute('data-category').includes(sortCattegory)){
       news.style.display = 'flex';
@@ -46,4 +53,7 @@ function categorySort(button){
   })
 }
 
-
+function redirectToDynamicPage(num){
+  const dynamicPageUrl = `dynamic_page.html?id=${num}`;
+  window.location.href = dynamicPageUrl;
+}
